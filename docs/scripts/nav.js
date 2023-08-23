0c7a4ce88c43465a1b3a40673732be783cd76a58
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2023-08-23 20:16:48 -0300

    Script para controlar rolagem ao abrir nav

--- docs/scripts/nav.js
+++ docs/scripts/nav.js
@@ -1,15 +1,21 @@
 function navMobile() {
+  //Variáveis
   let botaoAbrir = window.document.querySelector("button#menu-fechado")
   let botaoFechar = window.document.querySelector("button#menu-aberto")
   let nav = window.document.querySelector("nav")
+  let body = window.document.querySelector("body")
 
   if (botaoAbrir.style.display != "none") {
     botaoAbrir.style.display = "none"
     botaoFechar.style.display = "flex"
     nav.style.display = "flex"
+
+    body.style.overflow = "hidden" //Impede rolagem do conteúdo quando nav estiver aberto
   } else {
     botaoAbrir.style.display = "flex"
     botaoFechar.style.display = "none"
     nav.style.display = "none"
+
+    body.style.overflow = "auto" //Restaura rolagem ao fechar nav
   }
 }
